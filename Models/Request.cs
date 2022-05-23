98e00522d00d255f67b112e3d79b5a5ffa414c10
Ilya <serezha.mostyko@mail.ru>
2022-05-23 07:25:24 +0300

    request model

---
+++ Models/Request.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Autosalon.Models
+{
+    public partial class Request
+    {
+        public Request(Guid id, Guid automobileId, Guid userId)
+        {
+            Id = id;
+            AutomobileId = automobileId;
+            UserId = userId;
+        }
+        public Guid Id { get; set; }
+        public Guid AutomobileId { get; set; }
+        public Guid UserId { get; set; }
+
+        public virtual Automobile Automobile { get; set; } = null!;
+        public virtual Customer User { get; set; } = null!;
+    }
+}
